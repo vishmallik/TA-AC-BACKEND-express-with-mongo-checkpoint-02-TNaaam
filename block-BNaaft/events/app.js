8505a1c876a5a46e89e052e183b567ebf46bce58
visawjeet mallik <vishmallik@hotmail.com>
2022-10-08 20:05:35 +0530

    express with mongoDB checkpoint

---
+++ block-BNaaft/events/app.js
@@ -0,0 +1,53 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const lodash = require("lodash");
+
+const indexRouter = require("./routes/index");
+const eventsRouter = require("./routes/event");
+const remarksRouter = require("./routes/remark");
+
+mongoose.connect("mongodb://127.0.0.1/event", (err) => {
+  console.log(err ? err : "Connected to Database");
+});
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+  res.locals.lodash = lodash;
+  next();
+});
+
+app.use("/", indexRouter);
+app.use("/events", eventsRouter);
+app.use("/remarks", remarksRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
